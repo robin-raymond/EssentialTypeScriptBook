0e7d027579d7393aa67835c0d965ec7503470428
Jess Chadwick <jesschadwick@gmail.com>
2016-03-08 00:38:15 -0500

    Fixing Chapter 5

--- manuscript/code/test.ts
+++ manuscript/code/test.ts
@@ -1,9 +1,33 @@
-class TodoService {
-
-    constructor(private todos: Todo[]) {
-    }
-
-    getAll() {
-        return this.todos;
-    }
+enum TodoState {
+    New = 1,
+    Active = 2,
+    Complete = 3,
+    Deleted = 4
 }
+
+	class TodoStateChanger {
+	    
+	    constructor(private newState: TodoState) {
+	    }
+	    
+	    canChangeState(todo: Todo): boolean {
+	        return !!todo;
+	    }
+	    
+	    changeState(todo: Todo): Todo {
+	        if(this.canChangeState(todo)) {
+	            todo.state = this.newState;
+	        }
+	        
+	        return todo;
+	    }
+	    
+	}
+    
+    
+    
+    class CompleteTodoStateChanger extends TodoStateChanger {
+        constructor() {
+            super()
+        }
+	}
